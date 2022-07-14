90aa4517a9799231b984103d38e854742d08219e
AafreenKhan040896 <77886222+AafreenKhan040896@users.noreply.github.com>
2022-07-14 22:36:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var blastImage;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+
+function preload() {
+  
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
